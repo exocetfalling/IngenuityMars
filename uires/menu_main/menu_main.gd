485a238f2cdd8edc2b75c13b549ec50e8ae2c97a
exocetfalling <70193215+exocetfalling@users.noreply.github.com>
2023-08-13 19:08:29 +0800

    Bandaid loading fix

--- uires/menu_main/menu_main.gd
+++ uires/menu_main/menu_main.gd
@@ -8,30 +8,32 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	preload("res://scenes/test_scene.tscn")
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if($Options/ButtonFreeFlight.pressed == true):
+	if ($Options/ButtonFreeFlight.pressed == true):
 #		get_tree().change_scene("res://uires/menu_free_flight/menu_free_flight.tscn")
-#		$LoadingNotice.visible = true
-#		$Label.text = "FML"
+		$LoadingNotice.visible = true
+		$Label.text = "FML"
 		get_tree().change_scene("res://scenes/test_scene.tscn")
 	
-	if($Options/ButtonTutorials.pressed == true):
+	if ($Options/ButtonTutorials.pressed == true):
 		get_tree().change_scene("res://uires/menu_tutorials/menu_tutorials.tscn")
 	
-	if($Options/ButtonMissions.pressed == true):
+	if ($Options/ButtonMissions.pressed == true):
 		pass
 	
-	if($Options/ButtonCredits.pressed == true):
+	if ($Options/ButtonCredits.pressed == true):
 		get_tree().change_scene("res://uires/menu_credits/menu_credits.tscn")
 	
-	if($Options/ButtonExit.pressed == true):
+	if ($Options/ButtonExit.pressed == true):
 		get_tree().notification(MainLoop.NOTIFICATION_WM_QUIT_REQUEST)
 	
 	
 	if (get_tree().paused == true):
 		get_tree().paused = false
 	pass
+
