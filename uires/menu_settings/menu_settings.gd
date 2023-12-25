577bbc45ede5e7bc80add6d0018a94bf5261a785
toffee2000 <toffee2000@gmail.com>
2023-12-24 18:59:08 -1000

    Added code outline

--- uires/menu_settings/menu_settings.gd
+++ uires/menu_settings/menu_settings.gd
@@ -1,23 +1,23 @@
-extends Node
+extends Control
 
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
-# Setting, 0-3
-# 0 -> off
-# 1 -> low
-# 2 -> med
-# 3 -> high
-export var opt_shadows: int = 2
-export var opt_dust_effects: int = 2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+#	preload("res://scenes/test_scene.tscn")
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	if ($Options/ButtonShadows.pressed == true):
+		pass
+	
+	if ($Options/ButtonDustEffects.pressed == true):
+		pass
+
+	pass
