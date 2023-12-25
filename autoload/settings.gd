d89d34cb60988c077c04a3d1a524c67a6f92354f
toffee2000 <toffee2000@gmail.com>
2023-12-24 18:34:27 -1000

    Added settings script

---
+++ autoload/settings.gd
@@ -0,0 +1,23 @@
+extends Node
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Setting, 0-3
+# 0 -> off
+# 1 -> low
+# 2 -> med
+# 3 -> high
+export var opt_shadows: int = 2
+export var opt_dust: int = 2
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
