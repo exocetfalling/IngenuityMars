792f42d98e77bfa642fb8445a2258cbcd06a063a
toffee2000 <toffee2000@gmail.com>
2023-12-27 18:24:25 -1000

    Add load/save from local file

--- uires/menu_settings/menu_settings.gd
+++ uires/menu_settings/menu_settings.gd
@@ -8,30 +8,69 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-#	preload("res://scenes/test_scene.tscn")
-	pass # Replace with function body.
+	var score_data = {}
+	var config = ConfigFile.new()
 
+	# Load data from a file.
+	var err = config.load("user://settings.ini")
 
+	# If the file didn't load, ignore it.
+	if err != OK:
+		return
+
+	# Load data
+	Settings.opt_dust_effects = config.get_value("graphics", "opt_dust_effects")
+	Settings.opt_shadows = config.get_value("graphics", "opt_shadows")
+	
+	# Push in dust effect button corresponding to setting on load
+	if Settings.opt_dust_effects == 0:
+		$Panes/OptionsDust/ButtonOff.pressed = true
+	if Settings.opt_dust_effects == 1:
+		$Panes/OptionsDust/ButtonLow.pressed = true
+	if Settings.opt_dust_effects == 2:
+		$Panes/OptionsDust/ButtonMedium.pressed = true
+	if Settings.opt_dust_effects == 3:
+		$Panes/OptionsDust/ButtonHigh.pressed = true
+	
+	if Settings.opt_shadows == 0:
+		$Panes/OptionsShadows/ButtonOff.pressed = true
+	if Settings.opt_shadows == 1:
+		$Panes/OptionsShadows/ButtonLow.pressed = true
+	if Settings.opt_shadows == 2:
+		$Panes/OptionsShadows/ButtonMedium.pressed = true
+	if Settings.opt_shadows == 3:
+		$Panes/OptionsShadows/ButtonHigh.pressed = true
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if ($Options/ButtonOff.pressed == true):
+	if ($Panes/OptionsDust/ButtonOff.pressed == true):
 		Settings.opt_dust_effects = 0
-		Settings.opt_shadows = 0
-	
-	if ($Options/ButtonLow.pressed == true):
+	if ($Panes/OptionsDust/ButtonLow.pressed == true):
 		Settings.opt_dust_effects = 1
-		Settings.opt_shadows = 1
-	
-	if ($Options/ButtonMedium.pressed == true):
+	if ($Panes/OptionsDust/ButtonMedium.pressed == true):
 		Settings.opt_dust_effects = 2
-		Settings.opt_shadows = 2
-	
-	if ($Options/ButtonHigh.pressed == true):
+	if ($Panes/OptionsDust/ButtonHigh.pressed == true):
 		Settings.opt_dust_effects = 3
-		Settings.opt_shadows = 3
 	
+	if ($Panes/OptionsShadows/ButtonOff.pressed == true):
+		Settings.opt_shadows = 0
+	if ($Panes/OptionsShadows/ButtonLow.pressed == true):
+		Settings.opt_shadows = 1
+	if ($Panes/OptionsShadows/ButtonMedium.pressed == true):
+		Settings.opt_shadows = 2
+	if ($Panes/OptionsShadows/ButtonHigh.pressed == true):
+		Settings.opt_shadows = 3
+
 	if ($ButtonBack.pressed == true):
 		get_tree().change_scene("res://uires/menu_main/menu_main.tscn")
 	
 	if ($ButtonConfirm.pressed == true):
-		pass
+		# Create new ConfigFile object.
+		var config = ConfigFile.new()
+
+		# Store some values.
+		config.set_value("graphics", "opt_dust_effects", Settings.opt_dust_effects)
+		config.set_value("graphics", "opt_shadows", Settings.opt_shadows)
+
+		# Save it to a file (overwrite if already exists).
+		config.save("user://settings.ini")
