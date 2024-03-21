982186e4b0ad8fbf352280f2b822604196e702be
exocetfalling <70193215+exocetfalling@users.noreply.github.com>
2024-03-21 10:52:20 +0800

    Add mission goals enum

--- missions/mission.gd
+++ missions/mission.gd
@@ -6,10 +6,13 @@ class_name Mission
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+var goals_array: Array
 export var wpt_array: PoolVector3Array = [Vector3.ZERO]
-export var text_title: String
-export var text_goals: PoolStringArray
 
+export var title_text: String
+export var goals_text: PoolStringArray
+
+enum goal_type {LOCATION, TIME, WAIT}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
