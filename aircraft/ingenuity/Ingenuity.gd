9dc8b71442d7831cac2e63df7afd8b1e9859e0f4
toffee2000 <toffee2000@gmail.com>
2024-12-24 17:06:43 +0800

    Add CAS messages update logic

--- aircraft/ingenuity/Ingenuity.gd
+++ aircraft/ingenuity/Ingenuity.gd
@@ -127,6 +127,20 @@ func map_vector_square_to_circle(vector_square: Vector2):
 	return vector_circle
 
 
+func update_cas_messages(message_array: PoolStringArray):
+	if AeroDataBus.aircraft_battery_level <= 25:
+		message_array.append("BATT LOW")
+	if AeroDataBus.aircraft_alt_agl >= 25:
+		message_array.append("ABOVE MAX ALT")
+	
+	$HUDShared/Messages.text = ""
+	
+	for msg in message_array:
+		$HUDShared/Messages.text += msg + "\n"
+	
+	message_array = []
+
+
 # Called every physics frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta): 
 	if (control_type == 1):
